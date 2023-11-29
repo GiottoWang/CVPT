89d990b99d5d5e06651684465c1f46eae0def429
Wang Jianwei <50682698@qq.com>
2023-11-29 14:47:25 +0800

    Create PreprocessingAlgorithm.h

---
+++ ConsoleApplication1/PreprocessingAlgorithm.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <opencv.hpp>
+#include <map>
+#include<vector>
+#include <algorithm>
+#include <numeric>
+class PreprocessingAlgorithm {
+public:
+	virtual ~PreprocessingAlgorithm()=default;
+	virtual cv::Mat process()=0;
+};
+class PreprocessingAlgorithm_A:public PreprocessingAlgorithm
+{
+public:
+	typedef int channel_t;
+
+	//构造函数
+	PreprocessingAlgorithm_A() = default;
+	PreprocessingAlgorithm_A(cv::Mat a) :data(a) {};
+
+	//将RGB图像转换为灰度图像的函数
+	cv::Mat process();
+	
+private:
+	//定义一些常量来替代魔法数字
+	constexpr static  channel_t B = 0;
+	constexpr static  channel_t G = 1;
+	constexpr static  channel_t R = 2;
+	constexpr static  int THRESHOLD_WEIGHT = 20;
+	constexpr static double THRESHOLD_NOISE = 0.03;
+	
+	//输入图像
+	cv::Mat data;
+	//输出图像
+	cv::Mat output;
+	//
+	bool FLAG_INVERT = true;
+
+	//三通道灰度占比统计量结构体
+	struct count_t {
+		channel_t channel;
+		double weight;
+	};
+	struct spot_t {
+		int x;
+		int y;
+		int num;
+	};
+	struct effective_t {
+		int x;
+		int y_left;
+		int y_right;
+	};
+
+	//计算图像的补偿权重
+	void calculateWeight(cv::Mat img_channels[3], double weight[3]);
+
+	//转换图像的色彩空间
+	void convertColor();
+
+	//对灰度图像进行去噪
+	void deNoise();
+
+	//对低曝光图像进行取反
+	void invertion();
+};
+
